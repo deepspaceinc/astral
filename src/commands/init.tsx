6efdb4fec92306b508c8f311fba47401e217175c
joe <jlachance1@gmail.com>
2025-05-17 20:11:20 -0500

    Init working, refactored the flow and logs.

--- src/commands/init.tsx
+++ src/commands/init.tsx
@@ -1,7 +1,7 @@
-import { useEffect } from 'react';
-import yoctoSpinner from 'yocto-spinner';
-import { checkDependencies } from '../utils/terminal.js';
+import React, { useEffect, useState } from 'react';
+import { checkDependencies } from '../utils/dependencies.js';
 import { checkInit, initDotAstral, initLanguageFiles, getLanguage } from '../utils/init.js';
+import { Spinner, StatusMessage } from '@inkjs/ui';
 
 /**
  * Generates files for first time users.
@@ -10,39 +10,90 @@ import { checkInit, initDotAstral, initLanguageFiles, getLanguage } from '../uti
  * @returns {null}
  */
 export default function Init() {
+	const [files, setFiles] = useState<boolean>(false);
+	const [filesStatus, setFilesStatus] = useState<string>('');
+	const [filesDone, setFilesDone] = useState<boolean>(false);
+
+	const [language, setLanguage] = useState<boolean>(false);
+	const [languageStatus, setLanguageStatus] = useState<string>('');
+	const [languageDone, setLanguageDone] = useState<boolean>(false);
+
+	const [dependencies, setDependencies] = useState<boolean>(false);
+	const [dependenciesStatus, setDependenciesStatus] = useState<string>('');
+	const [dependenciesDone, setDependenciesDone] = useState<boolean>(false);
+
+	// Check .astral files & dependencies. If they don't exist, add 'em!
 	useEffect(() => {
-		// Check .astral files. If they don't exist, add them
-		const spinner = yoctoSpinner({ text: 'checking astral install...' }).start();
-		setTimeout(() => {
+		// Main initialization function to coordinate all tasks
+		const initializeAll = async () => {
+			// 1. Check Astral files
+			setFiles(true);
+			setFilesStatus('setting up astral...');
 			const hasAstralFiles = checkInit();
 			if (!hasAstralFiles) {
-				spinner.text = 'astral not found, installing astral...';
 				initDotAstral();
-				const language = getLanguage();
-				if (language) {
-					spinner.text = 'initializing language files...';
-					initLanguageFiles(language);
-				}
+				setFiles(false);
+				setFilesDone(true);
+				setFilesStatus('astral files created 💪');
+			} else {
+				setFiles(false);
+				setFilesDone(true);
+				setFilesStatus('astral already set up');
 			}
 
-			spinner.success('astral installed');
-		}, 200);
+			// 2. Check language files
+			setLanguage(true);
+			const language = getLanguage();
+			if (!hasAstralFiles && language) {
+				setLanguageStatus('setting up language files...');
+				initLanguageFiles(language);
 
-		// Check dependencies
-		(async function checkDeps() {
-			// Check Pulumi Installation. If not installed, install.
-			const spinner = yoctoSpinner({ text: 'checking dependencies...' }).start();
-			setTimeout(async () => {
-				const deps = await checkDependencies();
-				const notInstalled = deps.filter((dep) => dep.isInstalled);
-				if (notInstalled.length > 0) {
-					spinner.text = 'Installing dependencies...';
-					// TODO: Install dependencies
-				}
+				setLanguage(false);
+				setLanguageDone(true);
+				setLanguageStatus('language files created');
+			} else {
+				setLanguage(false);
+				setLanguageDone(true);
+				setLanguageStatus('language files already set up');
+			}
 
-				spinner.success('dependencies checked');
-			}, 200);
-		})();
+			// 3. Check dependencies
+			setDependencies(true);
+			setDependenciesStatus('checking dependencies...');
+
+			// Process dependencies check
+			const deps = await checkDependencies();
+			const notInstalled = deps.filter((dep) => dep.isInstalled === false);
+			if (notInstalled.length > 0) {
+				setDependenciesStatus('Installing dependencies...');
+				// TODO: Install dependencies
+			}
+
+			setDependencies(false);
+			setDependenciesDone(true);
+			setDependenciesStatus('dependencies installed');
+		};
+		initializeAll();
 	}, []);
-	return null;
+	return (
+		<>
+			{files ? <Spinner label={filesStatus} /> : null}
+			{filesDone && <StatusMessage variant="success">{filesStatus}</StatusMessage>}
+
+			{language ? <Spinner label={languageStatus} /> : null}
+			{languageDone && <StatusMessage variant="success">{languageStatus}</StatusMessage>}
+
+			{dependencies ? <Spinner label={dependenciesStatus} /> : null}
+			{dependenciesDone && <StatusMessage variant="success">{dependenciesStatus}</StatusMessage>}
+
+			{filesDone && languageDone && dependenciesDone && (
+				<>
+					<StatusMessage variant="success">{'Astral installed'}</StatusMessage>
+					<StatusMessage variant="success">
+						{'Check your configuration in astral.deploy.js and run deploy 🎉'}
+					</StatusMessage>
+				</>
+			)}
+		</>
+	);
 }
